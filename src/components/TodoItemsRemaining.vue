81efbae0fae0e091c1b7e9a47a89e14127be7a90
Cuong Nguyen <cuongnguyen.hn96@gmail.com>
2021-08-21 21:13:09 +0700

    first commit

---
+++ src/components/TodoItemsRemaining.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>{{ remaining }} items left</div>
+</template>
+
+<script>
+export default {
+  name: 'todo-remaining',
+  created() {
+    eventBus.$on('pluralize', this.handlePluralize)
+  },
+  beforeDestroy() {
+    eventBus.$off('pluralize', this.handlePluralize)
+  },
+  methods: {
+    handlePluralize() {
+      console.log(1233);
+      this.$store.dispatch('addTodo', {
+        'id': this.todosFiltered.length + 1,
+        'title': "haha",
+      })
+    }
+  },
+  computed: {
+    remaining() {
+      return this.$store.getters.remaining
+    },
+    todosFiltered() {
+      return this.$store.getters.todosFiltered
+    }
+  }
+}
+</script>
+
